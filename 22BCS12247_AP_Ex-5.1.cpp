a7d7e8c06942b97d58b80b4d434e984056323f6d
Hemant Vyas <110166331+8anu@users.noreply.github.com>
2025-02-19 15:56:17 +0530

    Add files via upload

---
+++ 22BCS12247_AP_Ex-5.1.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void sortColors(vector<int>& nums) {
+    int l = 0;
+    int mid = 0;
+    int high = nums.size() - 1;
+
+    while (mid <= high) {
+        if (nums[mid] == 0) {
+            swap(nums[mid], nums[l]);
+            mid++;
+            l++;
+        }
+        else if (nums[mid] == 1) {
+            mid++;
+        }
+        else {
+            swap(nums[mid], nums[high]);
+            high--;
+        }
+    }
+}
+
+int main() {
+    vector<int> nums = {2, 0, 2, 1, 1, 0};
+
+    cout << "Before sorting: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    sortColors(nums);
+
+    cout << "After sorting: ";
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
